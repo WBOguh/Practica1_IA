f32504e23f59d03f8461233a6449e8d266bc65bf
WBOguh <hugobejarmurcia@gmail.com>
2024-03-12 18:44:54 +0100

    Tutorial cais acabado segmentation core arreglado

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -55,10 +55,10 @@ Action ComportamientoJugador::think(Sensores sensores)
 				case sur: current_state.fil+=2; break;
 				case este: current_state.col+=2; break;
 				case oeste: current_state.col-=2; break;
-				case noreste: current_state.fil--; current_state.col++; break;
-				case noroeste: current_state.fil--; current_state.col--; break;
-				case sureste: current_state.fil++; current_state.col++; break;
-				case suroeste: current_state.fil++; current_state.col--; break;
+				case noreste: current_state.fil-=2; current_state.col+=2; break;
+				case noroeste: current_state.fil-=2; current_state.col-=2; break;
+				case sureste: current_state.fil+=2; current_state.col+=2; break;
+				case suroeste: current_state.fil+=2; current_state.col-=2; break;
 			}
 			break;
 		case actTURN_SR:
@@ -68,21 +68,41 @@ Action ComportamientoJugador::think(Sensores sensores)
 			break;
 		case actTURN_L:
 			a = current_state.brujula;
-			a = (a + 7) % 8;
+			a = (a + 6) % 8;
 			current_state.brujula = static_cast<Orientacion>(a);
 			break;
 	}
-
-if (sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' and sensores.agentes[2] == '_'){
+if (sensores.terreno[0]=='G' and !bien_situado){
+	current_state.fil = sensores.posF;
+	current_state.col= sensores.posC;
+	current_state.brujula = sensores.sentido;
+	bien_situado = true;
+}
+if (bien_situado){
+	PonerTerrenoMatriz(sensores.terreno, current_state, mapaResultado);
+	}
+if (sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' or sensores.terreno[2] == 'G' and sensores.agentes[2] == '_'){
 	accion = actWALK;
-	}else 
+	}else if(!girar_derecha){
 		accion = actTURN_L;
+		girar_derecha = (rand() % 2 == 0);
+	}else{
+		accion = actTURN_SR;
+		girar_derecha = (rand() % 2 == 0);
+	}
 
 last_action = accion;	
 	return accion;
 }
-
+void ComportamientoJugador::PonerTerrenoMatriz(const vector<unsigned char> &terreno, const state &st, vector <vector< unsigned char> > &matriz){
+    if (st.fil < 0 || st.fil >= matriz.size() || st.col < 0 || st.col >= matriz[0].size()) {
+        cout << "Error: Índices fuera de los límites de la matriz." << endl;
+        return;
+    }
+    matriz[st.fil][st.col] = terreno[0];
+}
 int ComportamientoJugador::interact(Action accion, int valor)
 {
 	return false;
 }
+
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -16,6 +16,8 @@ class ComportamientoJugador : public Comportamiento{
       current_state.fil = 99;
       current_state.col = 99;
       current_state.brujula = norte;
+      girar_derecha = false;
+      bien_situado = false;
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -23,11 +25,13 @@ class ComportamientoJugador : public Comportamiento{
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
-
+    void PonerTerrenoMatriz(const vector<unsigned char> &terreno, const state &st, vector <vector< unsigned char> > &matriz);
   private:
   // Declarar aquí las variables de estado
   state current_state;
   Orientacion brujula;
   Action last_action;
+  bool girar_derecha;
+  bool bien_situado;
 };
 #endif
